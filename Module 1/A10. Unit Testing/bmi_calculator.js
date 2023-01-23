b0008640f70aa246d9b965fcd2524ab308c364af
JV Faeldon <jonelfaeldon@gmail.com>
2023-01-23 16:31:28 +0800

    restructure folder items for module 1

---
+++ Module 1/A10. Unit Testing/bmi_calculator.js
@@ -0,0 +1,23 @@
+function calculateBMI(weight, height){
+    // calculates the BMI
+    let bmi = (weight / Math.pow( (height/100), 2 )).toFixed(1);
+
+    // check BMI status
+    if(bmi < 18.5){
+        bmiStatus = `STATUS: Underweight`;
+    }
+
+    else if( bmi >= 18.5 && bmi <= 24.9 ){
+        bmiStatus = `STATUS: Normal`;
+    }
+
+    else if( bmi >= 25 && bmi <= 29.9 ){
+        bmiStatus = `STATUS: Overweight`;
+    }
+
+    else{
+        bmiStatus = `STATUS: Obese`
+    }
+
+    return `BMI: ${bmi}\n${bmiStatus}`
+}
